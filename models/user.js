b73dc403a5e59e38fcb5218a73d129e2a1a137f0
kourtesimaria <maria.kourtesi@cdl.co.uk>
2021-06-29 13:43:30 +0100

    added user Schema

---
+++ models/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    description: { type: String }
+});
+
+module.exports = mongoose.model('user', userSchema);
